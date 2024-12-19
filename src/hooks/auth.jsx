cef338627876ede87b699f3021f8b7c5e025fc00
Rafaa Ehlke <heeyrafaa@gmail.com>
2024-12-18 22:16:23 -0300

    :construction: adicionado comentarios

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -12,8 +12,9 @@ function AuthProvider({ children }) {
       const response = await api.post("/sessions", { email, password })
       const { user, token } = response.data
 
+      // Inseri um token do tipo Bearer no cabeçalho de todas as requisicoes que o usuario ira fazer
       api.defaults.headers.authorization = `Bearer ${token}`
-      setData({ user, token })
+      setData({ user, token }) // armazena as informacoes do usuario, dentro do state
 
     } catch (e) {
       if (e.response) {
