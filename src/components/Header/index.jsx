48b853f49aeac512327ea59ba4b38ea56fe2235d
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-01-06 21:11:13 -0300

    :construction: Logout apartir de uma nota aberta, direcionado para HOME

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,19 @@
 import avatarPlaceholder from '../../assets/avatar_placeholder.svg'
 import { Container, Profile, Logout } from "./styles";
 import { RiShutDownLine } from 'react-icons/ri'
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from "../../hooks/auth"
 import { api } from '../../services/api';
 
 export function Header() {
   const { signOut, user } = useAuth()
+  const navigate = useNavigate()
+  
+  function handleSignOut() {
+    navigate("/")
+    signOut()
+  }
+
   const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
   return (
@@ -22,7 +30,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
