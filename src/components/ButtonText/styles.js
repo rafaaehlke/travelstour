cfa47e6f9393134e45597e90d0ac121aa0c1d491
Rafaa Ehlke <heeyrafaa@gmail.com>
2025-01-06 21:32:15 -0300

    :recycle: corrido cor dos botoes

--- src/components/ButtonText/styles.js
+++ src/components/ButtonText/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.button`
   background: none;
-  color: ${({ theme, $isactive }) =>  $isactive ? theme.COLORS.BLUE : theme.COLORS.BACKGROUND_700};
+  color: ${({ theme, $isactive }) =>  $isactive ? theme.COLORS.BLUE : theme.COLORS.GRAY_300};
 
   border: none;
   font-size: 16px;
